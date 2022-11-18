41664e889fa38cb80aa97a77fd8faaf06a81d2a0
AsharMallick <asharmallick750@gmail.com>
2022-11-18 18:16:21 +0500

    Added Copy to clipboard functionality

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import React, { useState } from 'react'
 import axios from 'axios';
 const App = () => {
+    function copyDivToClipboard() {
+        var range = document.createRange();
+        range.selectNode(document.getElementById("a"));
+        window.getSelection().addRange(range); // to select text
+        document.execCommand("copy");
+        window.getSelection().removeAllRanges();// to deselect
+    }
     const [joke, setJoke] = useState()
     const [id, setId] = useState()
     const fetchData = async () => {
@@ -10,24 +17,18 @@ const App = () => {
             }
         })
         const jokeobj = await data.json()
-        console.log(jokeobj)
+        // console.log(jokeobj)
         setJoke(jokeobj.joke)
         setId(jokeobj.id)
     }
     return (
-        // <div>
-        //     <div className="card" style={{width:"18rem"}}>
-        //         <div className="card-body">
-        //            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        //         </div>
-        //     </div>
-        // </div>
         <div className='h-100' style={{ height: "100vh" }}>
             <div className="d-flex justify-content-center align-items-center" style={{ position: "relative", top: "300px" }}>
                 <div className="card" style={{ width: "18rem" }}>
                     <div className="card-body">
-                        <h5 className="card-title">{joke}</h5>
+                        <h5 className="card-title" id='a'>{joke}</h5>
                         <p className="card-text">joke id: {id}</p>
+                        <button className="btn btn-primary" onClick={copyDivToClipboard}>Copy</button>
                         <button className="btn btn-success text-align-center" style={{ margin: 'auto', display: "inherit", width: '100%' }} onClick={fetchData}>Click for a joke</button>
                     </div>
                 </div>
