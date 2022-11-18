60ef2690464e5884dafa2db174f9be75d94c3ec1
AsharMallick <asharmallick750@gmail.com>
2022-11-18 17:11:50 +0500

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React, { Component} from 'react'
-import "./App.css"
+import React, { Component } from 'react'
+// import "./App.css"
 class App extends Component {
-    
     state = {
-        joke: ""
+        joke: "",
+        id:0
     }
     componentDidMount() {
         this.fetchData()
@@ -15,24 +15,32 @@ class App extends Component {
             }
         })
         const jokeobj = await data.json()
-        console.log(jokeobj.joke)
+        console.log(jokeobj)
         this.setState({
-            joke: jokeobj.joke
-        } )
+            joke: jokeobj.joke,
+            id:jokeobj.id
+        })
     }
     render() {
         return (
-            <>
-                <header><h1 style={{ textAlign: 'center' }}>Welcome to dad jokes app</h1></header>
-                    <div className="app">
-                        <div className="card">
-                        <h1 className="heading">{this.state.joke? this.state.joke:"Internel server error"}</h1>
-                            <button className="button" onClick={this.fetchData}>
-                                <span>New joke</span>
-                            </button>
-                        </div>
-                    </div>
-            </>
+            // <div>
+            //     <div className="card" style={{width:"18rem"}}>
+            //         <div className="card-body">
+            //            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            //         </div>
+            //     </div>
+            // </div>
+            <div className='h-100' style={{height:"100vh"}}>
+            <div className="d-flex justify-content-center align-items-center" style={{position:"relative", top:"300px"}}>
+            <div className="card" style={{width: "18rem"}}>
+                <div className="card-body">
+                    <h5 className="card-title">{this.state.joke}</h5>
+                    <p className="card-text">joke id: {this.state.id}</p>
+                    <button className="btn btn-success text-align-center" style={{ margin:'auto', display:"inherit", width:'100%'}} onClick={this.fetchData}>New Joke</button>
+                </div>
+                </div>
+                </div>
+            </div>
         )
     }
 }
